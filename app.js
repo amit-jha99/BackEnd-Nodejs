179ddf8c2195900ccd188d799ea3abc43eec7eee
amit-jha99 <amitjha121299@gmail.com>
2024-08-28 14:33:11 +0530

    Let's write code

---
+++ app.js
@@ -0,0 +1,12 @@
+var name ="Amit Jha";
+
+var a =100;
+var b =500;
+
+
+// console.log(name);
+// console.log(a+b);
+// console.log(global);
+// console.log(this) // it prints an empty object
+console.log(globalThis);
+console.log(globalThis === global);
