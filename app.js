e4f11d1909605a9809d8a0c97e3a4fa3dcd2123b
amit-jha99 <amitjha121299@gmail.com>
2024-08-28 15:18:01 +0530

    module.export and require

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+require("./xyz.js");// one module into another 
 var name ="Amit Jha";
+const {x,calculateSum} = require("./sum.js");
 
 var a =100;
 var b =500;
+
+// obj.calculateSum(a,b);
+calculateSum(a,b);
+console.log(x)
 
 
 // console.log(name);
 // console.log(a+b);
 // console.log(global);
 // console.log(this) // it prints an empty object
-console.log(globalThis);
+// console.log(globalThis);
 console.log(globalThis === global);
---
+++ sum.js
@@ -0,0 +1,12 @@
+//important:->Modules protects their variables and functions from leaking
+console.log("Sum module executed");
+var x = "Hello World";
+function calculateSum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+module.exports = {
+  x,
+  calculateSum,
+};
