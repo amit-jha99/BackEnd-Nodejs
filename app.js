06542ed387dab392611c2329151e811429ceb797
amit-jha99 <amitjha121299@gmail.com>
2024-08-28 18:12:32 +0530

    Nested Modules

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require("./xyz.js");// one module into another 
 var name ="Amit Jha";
-const {x,calculateSum} = require("./sum.js");
+// const {x,calculateSum} = require("./sum.js");
+// import { x,calculateSum } from "./sum.js";
+// const {calculateMultiply} = require('./calculate/multiply.js');
+
+const {calculateMultiply,calculateSum} = require("./calculate");
+const data = require("./data.json");
 
 var a =100;
 var b =500;
 
 // obj.calculateSum(a,b);
 calculateSum(a,b);
-console.log(x)
+calculateMultiply(a,b);
+// console.log(x)
+// var x =100;
 
 
 // console.log(name);
@@ -16,3 +23,4 @@ console.log(x)
 // console.log(this) // it prints an empty object
 // console.log(globalThis);
 console.log(globalThis === global);
+console.log(data);
--- calculate/sum.js
+++ calculate/sum.js
@@ -2,10 +2,12 @@
 console.log("Sum module executed");
 var x = "Hello World";
 function calculateSum(a, b) {
+    // z ="escape the matrix";
   const sum = a + b;
   console.log(sum);
 }
 
+// console.log(module.exports)
 module.exports = {
   x,
   calculateSum,
